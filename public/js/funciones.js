707d5faf7515af20f5511b0af890a046e55392cd
Tomas <j35c7z@gmail.com>
2023-10-05 15:14:51 -0500

    actualizacion-proyecto

--- public/js/funciones.js
+++ public/js/funciones.js
@@ -3,44 +3,47 @@ const nav = document.querySelector('nav');
     window.addEventListener('scroll', function(){
         nav.classList.toggle('active', this.window.scrollY > 0)
     })
+$(document).ready(function () {
+    var indice = 0;
 
-var indice=0;
-$(".pagination li").eq(indice).css("background-color","white");
-$(".pagination li").click(function(){
-    
-    indice=$(this).index();
-    $(".pagination li").css("background-color","black");
-    $(this).css("background-color","white");
-    $(".slide").hide();
-    $(".slide").eq(indice).fadeIn();
-});
-setInterval(function(){
-    indice++;
-    if(indice>2){
-        indice=0;
+    function mostrarSlide() {
+        $(".slide").removeClass("active"); // Ocultamos todas las imágenes
+        $(".pagination li").css("background-color", "black");
+
+        $(".slide")
+        .eq(indice)
+        .addClass("active"); // Mostramos la imagen activa
+        $(".pagination li").eq(indice).css("background-color", "white");
     }
-    $(".slide").hide();
-    $(".slide").eq(indice).fadeIn();
-    $(".pagination li").css("background-color","black");
-    $(".pagination li").eq(indice).css("background-color","white");
-},100000);
-$(".right").click(function(){
-    indice++;
-    if(indice>2){
-        indice=0;
+
+    $(".pagination li").eq(indice).css("background-color", "white");
+
+    $(".pagination li").click(function () {
+        indice = $(this).index();
+        mostrarSlide();
+    });
+
+    function avanzarSlide() {
+        indice++;
+        if (indice > 2) {
+        indice = 0;
+        }
+        mostrarSlide();
     }
-    $(".slide").hide();
-    $(".slide").eq(indice).fadeIn();
-    $(".pagination li").css("background-color","black");
-    $(".pagination li").eq(indice).css("background-color","white");
-});
-$(".left").click(function(){
-    indice--;
-    if(indice<0){
-        indice=2;
+
+    function retrocederSlide() {
+        indice--;
+        if (indice < 0) {
+        indice = 2;
+        }
+        mostrarSlide();
     }
-    $(".slide").hide();
-    $(".slide").eq(indice).fadeIn();
-    $(".pagination li").css("background-color","black");
-    $(".pagination li").eq(indice).css("background-color","white");
+
+    // Llamar a la función para mostrar la primera imagen
+    mostrarSlide();
+
+    setInterval(avanzarSlide, 7000);
+
+    $(".right").click(avanzarSlide);
+    $(".left").click(retrocederSlide);
 });
